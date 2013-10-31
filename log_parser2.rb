f266098301df0e462895f2e9e61122b4b03c2e6c
aruneapachenmec <arune@qburst.com>
2013-10-31 17:21:41 +0530

    Update log_parser2.rb

--- log_parser2.rb
+++ log_parser2.rb
@@ -18,99 +18,100 @@ class LogParser
     @file = File.read(path)
     @hash = Hash.new(0)
     @max = 0
-@max1 = 0
-	end	
+    @max1 = 0
+  end	
 
 =begin
-	Generates the grouped request and the maximum time taken by url.
-	input : @file
-	output : @hash, @max
+  Generates the grouped request and the maximum time taken by url.
+  input : @file
+  output : @hash, @max
 =end
 
-	def file_check
-		@file.each_line do |line|
-			#finding the number of request based on time.
-			match_true = /\d{1,2}\/[A-Z][a-z][a-z]\/\d{4}\:\d{1,2}/.match(line)
-			if match_true
-				@hash[match_true[0]] += 1
-			end
-			#finding the max request time. 
-			time = /\d+$/.match(line)
-			if time
-			 	temp = time[0].to_i	
-				if (temp) > @max
-			 		@max = temp
-			 	end
-			end	 
-		end
-		self
-	end
+  def file_check
+    @file.each_line do |line|
+      #finding the number of request based on time.
+      match_true = /\d{1,2}\/[A-Z][a-z][a-z]\/\d{4}\:\d{1,2}/.match(line)
+      if match_true
+        @hash[match_true[0]] += 1
+      end
+      #finding the max request time. 
+      time = /\d+$/.match(line)
+      if time
+        temp = time[0].to_i	
+        if (temp) > @max
+          @max = temp
+        end
+      end	 
+    end
+    self
+  end
 
 =begin
-	Prints the grouped request by time and date, and also finds the slowest url in each time group
-	and print them.
-	input : @hash
-	output : nil
+  Prints the grouped request by time and date, and also finds the slowest url in each time group
+  and print them.
+  input : @hash
+  output : nil
 =end
 
-	def number_request
-		@hash.each do |val1,val2|
-			$x = val1
+  def number_request
+    @hash.each do |val1,val2|
+      $x = val1
 
-			if val2 == 1 
-				req = "request"
-			elsif val2 > 1
-				req = "requests"
-			end
+      if val2 == 1 
+        req = "request"
+      elsif val2 > 1
+        req = "requests"
+      end
 				
-			puts "#{val1}:#{val2} #{req}"
-			puts "\n"
-			#finding the maximum time in a given time group
-			@file.each_line do |line|
-				if line.include? $x
-				  time = /\d+$/.match(line)
-				  temp = time[0].to_i	
-					if (temp) > @max1
-				 		@max1 = temp	
-				 end
-				end
-			end
-			
-			@file.each_line do |line|
-			# finding the url of the line with maximum request time.
-			if line.include? @max1.to_s && $x
-				url =  /"https?:\/\/[\S]+/.match(line)
-				if url 
-					puts "Slowest url in #{$x} is   #{url[0]} : #{@max1} secs"
-					puts "\n"
-					break
-				end
-			end
-		end
-			@max1 = 0
-		end
-		self
+      puts "#{val1}:#{val2} #{req}"
+      puts "\n"
+      #finding the maximum time in a given time group
+      @file.each_line do |line|
+        if line.include? $x
+          time = /\d+$/.match(line)
+          temp = time[0].to_i	
+          if (temp) > @max1
+	    @max1 = temp	
+          end
 	end
+      end
+
+      @file.each_line do |line|
+        # finding the url of the line with maximum request time.
+        if line.include? @max1.to_s && $x
+          url =  /"https?:\/\/[\S]+/.match(line)
+          if url 
+	    puts "Slowest url in #{$x} is   #{url[0]} : #{@max1} secs"
+	    puts "\n"
+	    break
+	  end
+	end			
+			
+      end
+      @max1 = 0
+    end
+    self
+  end
 
 =begin
-	Prints the url which took the maximum request time.
-	input : @max, @file
-	output : nil
+  Prints the url which took the maximum request time.
+  input : @max, @file
+  output : nil
 =end
 
-	def max_time_url
-		@file.each_line do |line|
-			# finding the url of the line with maximum request time.
-			if line.include? @max.to_s
-				url =  /"https?:\/\/[\S]+/.match(line)
-				if url 
-					puts "\n"
-					puts "Overall slowest url : #{url[0]} ie #{@max} secs"
-				end
-			end
-		end
-		self
-	end
+  def max_time_url
+    @file.each_line do |line|
+      # finding the url of the line with maximum request time.
+      if line.include? @max.to_s
+        url =  /"https?:\/\/[\S]+/.match(line)
+        if url 
+          puts "\n"
+	  puts "Overall slowest url : #{url[0]} ie #{@max} secs"
+        end
+      end
+    end
+  self
+  end
 
 
 end
